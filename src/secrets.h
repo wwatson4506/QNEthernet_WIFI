465138c555700e39e1ced8826975ef9d3e9b6834
Warren Watson <wwatson4506@gmail.com>
2026-07-08 18:55:05 -0700

    Add files via upload

--- src/secrets.h
+++ src/secrets.h
@@ -1,10 +1,10 @@
 // The secrets file
 
 // SSID
-#define MY_SSID            "yourSSID"
+#define MY_SSID            "wswn_eero"
 
 //PASSPHRASE
-#define MY_PASSPHRASE      "yourPWD"
+#define MY_PASSPHRASE      "Mirada4506#"
 
 // Security settings: 0 for none, 1 for WPA_TKIP, 2 for WPA2
 // The hard-coded password is for test purposes only!!!
