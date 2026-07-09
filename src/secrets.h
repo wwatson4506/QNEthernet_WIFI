0b83cd540b0a4ac8f9f0569f0655d54ca896f22f
Warren Watson <wwatson4506@gmail.com>
2026-07-08 17:32:04 -0700

    Update secrets.h

--- src/secrets.h
+++ src/secrets.h
@@ -1,10 +1,10 @@
 // The secrets file
 
 // SSID
-#define MY_SSID            "wswn_eero"
+#define MY_SSID            "yourSSID"
 
 //PASSPHRASE
-#define MY_PASSPHRASE      "Mirada4506#"
+#define MY_PASSPHRASE      "yourPWD"
 
 // Security settings: 0 for none, 1 for WPA_TKIP, 2 for WPA2
 // The hard-coded password is for test purposes only!!!
