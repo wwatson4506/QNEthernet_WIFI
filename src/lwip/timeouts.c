74792bb9a9b6f339d30c120b635026ea2a3e2565
Warren Watson <wwatson4506@gmail.com>
2026-07-25 08:39:28 -0700

    Add files via upload

--- src/lwip/timeouts.c
+++ src/lwip/timeouts.c
@@ -347,11 +347,8 @@ sys_untimeout(sys_timeout_handler handler, void *arg)
  * handler functions when timeouts expire.
  *
  * Must be called periodically from your main loop.
- *
- * @return the time left before the next timeout is due. If no timeouts are
- * enqueued, returns 0xffffffff
  */
-u32_t
+void
 sys_check_timeouts(void)
 {
   u32_t now;
@@ -370,13 +367,11 @@ sys_check_timeouts(void)
 
     tmptimeout = next_timeout;
     if (tmptimeout == NULL) {
-      return SYS_TIMEOUTS_SLEEPTIME_INFINITE;
+      return;
     }
 
     if (TIME_LESS_THAN(now, tmptimeout->time)) {
-      u32_t ret = (u32_t)(tmptimeout->time - now);
-      LWIP_ASSERT("invalid sleeptime", ret <= LWIP_MAX_TIMEOUT);
-      return ret;
+      return;
     }
 
     /* Timeout has expired */
