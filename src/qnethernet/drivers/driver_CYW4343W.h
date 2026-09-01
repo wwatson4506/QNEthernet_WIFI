8ca65349056974f2b0a77d7e763acb8d9fc279fd
Warren Watson <wwatson4506@gmail.com>
2026-08-31 21:39:40 -0700

    Update driver_CYW4343W.h

--- src/qnethernet/drivers/driver_CYW4343W.h
+++ src/qnethernet/drivers/driver_CYW4343W.h
@@ -2,8 +2,8 @@
 
 #pragma once
 
-#define MTU           1500
-#define MAX_FRAME_LEN 1518  /* Does not include the 4-byte FCS (frame check sequence) */
+#define MTU           4018 //1500
+#define MAX_FRAME_LEN 4018 // 1518 /* Does not include the 4-byte FCS (frame check sequence) */
 
 #define ETH_PAD_SIZE 0  /* 2 */
 #define EVENT_POLL_USEC    100000 //100000
